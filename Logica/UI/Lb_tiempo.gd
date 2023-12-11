d10a2a3f14f740aae6e27068558dbdc56f25209d
SebMatDo <59572442+SebMatDo@users.noreply.github.com>
2023-12-11 10:43:12 -0500

    Primer Commit de tiempo y recursos

---
+++ Logica/UI/Lb_tiempo.gd
@@ -0,0 +1,18 @@
+extends Label
+@onready var Lb_tiempo = %Lb_tiempo
+var strTiempo : String
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	GlobalTiempo.tiempoCambio.connect(actualizarTiempo)
+
+func actualizarTiempo(minuto, hora):
+	var strMin = ''
+	if minuto<10:
+		strMin = '0' + str(minuto)
+	else:
+		strMin = str(minuto)
+	if hora < 12:
+		strTiempo = str(hora) + ':' + strMin + ' AM'
+	else:
+		strTiempo = str(hora-12) + ':' + strMin + ' PM'
+	Lb_tiempo.text = strTiempo
