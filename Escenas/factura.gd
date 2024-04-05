954c90c87c2eb3641df002a60ee44b886276055e
Pab10Suarez <retroguias47@gmail.com>
2024-04-05 10:28:32 -0500

    animacionsello

--- Escenas/factura.gd
+++ Escenas/factura.gd
@@ -1,8 +1,18 @@
 extends Control
-
+var gameover:bool
 func _on_btn_factura_pressed():
-	%FinalizarDia.visible=true
+	
+	$AnimationPlayer.play("sello epico")
 	if GlobalRecursos.dinero<0:
+		$signaturepancha.frame=1
+		gameover=true
+	
+
+
+func pasar_de_pagina():
+	%FinalizarDia.visible=true
+	if gameover:
 		%Gameover.visible=true
 	self.visible=false
-
+	$signaturepancha.frame=0
+	$signaturepancha.visible=false
